0ca6c88dc281667bd11146f94335e886181f19de
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-04-22 14:18:04 +0000

    git-svn-id: https://svn.bluedynamics.eu/svn/module/bda.bfg.app/trunk@1920 f119a143-da5b-4457-81e3-bee00661a780

--- src/bda/bfg/app/views/static/bdajax.js
+++ src/bda/bfg/app/views/static/bdajax.js
@@ -11,7 +11,6 @@ jQuery(document).ready(function() {
 	jQuery().bdajax(true);
 });
 
-// bind bdajax behaviour to context
 jQuery.fn.bdajax = function(global) {
 	var context = this;
 	if (global) { context = null; }
@@ -32,8 +31,6 @@ jQuery.fn.bdajax = function(global) {
 
 bdajax = {
 	
-    // Display System message as overlay.
-    // @param message: Message to display as String or DOM element.
     message: function(message) {
         jQuery('#ajax-message').overlay({
             expose: {
@@ -49,8 +46,6 @@ bdajax = {
         }).load();
     },
     
-    // Display Error message as overlay.
-    // @param message: Message to display as String or DOM element.
     error: function(message) {
         jQuery("#ajax-message .message")
             .removeClass('error warning info')
@@ -59,8 +54,6 @@ bdajax = {
         bdajax.message(message);
     },
     
-    // Display Info message as overlay.
-    // @param message: Message to display as String or DOM element.
     info: function(message) {
         jQuery("#ajax-message .message")
             .removeClass('error warning info')
@@ -69,8 +62,6 @@ bdajax = {
         bdajax.message(message);
     },
     
-    // Display Warning message as overlay.
-    // @param message: Message to display as String or DOM element.
     warning: function(message) {
         jQuery("#ajax-message .message")
             .removeClass('error warning info')
@@ -79,8 +70,6 @@ bdajax = {
         bdajax.message(message);
     },
 	
-	// Strip query from URL if existent and return.
-	// @param url: Request URL as string.
 	parseurl: function(url) {
         var idx = url.indexOf('?');
         if (idx != -1) {
@@ -89,9 +78,6 @@ bdajax = {
 		return url;
 	},
 	
-    // Parse query string from URL if existent and return query parameters as
-    // object.
-    // @param url: Request URL as string.
 	parsequery: function(url) {
 		var params = {};
 		var idx = url.indexOf('?');
@@ -105,8 +91,6 @@ bdajax = {
 		return params;
 	},
 	
-	// Parse target definitions from ``ajax:target`` on elem
-	// @param target: DOM element with ``ajax:target`` attribute set
 	parsetarget: function(elem) {
 		var target = jQuery(elem).attr('ajax:target');
 		var url = bdajax.parseurl(target);
@@ -118,7 +102,6 @@ bdajax = {
 		};
 	},
 	
-	// Callback handler for javascript function calls.
     call: function(event) {
         event.preventDefault();
         var attr = jQuery(this).attr('ajax:call');
@@ -130,7 +113,6 @@ bdajax = {
         }
     },
 	
-	// Callback handler for event triggering.
     event: function(event) {
 		event.preventDefault();
         var target = bdajax.parsetarget(this);
@@ -144,7 +126,6 @@ bdajax = {
         }
     },
     
-    // Callback handler for an action binding.
     action: function(event) {
 		event.preventDefault();
 		var target;
@@ -166,7 +147,6 @@ bdajax = {
 		}
     },
 	
-    // Error messages.
     ajaxerrors: {
         timeout: 'The request has timed out. Pleasae try again.',
         error: 'An error occoured while processing the request. Aborting.',
@@ -174,22 +154,17 @@ bdajax = {
         unknown: 'An unknown error occoured while request. Aborting.'
     },
     
-    // Get error message.
-    // @param status: ``jQuery.ajax`` error callback status
     ajaxerror: function(status) {
         if (status == 'notmodified') { return; }
         if (status == null) { status = 'unknown' }
         return bdajax.ajaxerrors[status];
     },
 	
-	// Perform an ajax request.
-	// @param config: object containing request configuration.
-	//     Configuration fields:
-	//         success: Callback if request is successful.
-	//         url: Request url as string.
-	//         params: Query parameters for request as Object (optional). 
-	//         type: ``xml``, ``json``, ``script``, or ``html`` (optional).
-	//         error: Callback if request fails (optional).
+	// config.success: Callback if request is successful.
+	// config.url: Request url as string.
+	// config.params: Query parameters for request as Object (optional). 
+	// config.type: ``xml``, ``json``, ``script``, or ``html`` (optional).
+	// config.error: Callback if request fails (optional).
 	request: function(config) {
 		if (config.url.indexOf('?') != -1) {
 			var addparams = config.params;
@@ -217,17 +192,11 @@ bdajax = {
 	    });
 	},
 	
-	// Perform JSON request to server and alter element(s).
-	// This function expects as response an array containing a name
-	// mapping to class and/or id attributes of the dom element to alter
-	// and the html payload which is used as data replacement.
-	// @param config: object containing action configuration.
-    //     Configuration fields:
-    //         name: Action name
-    //         selector: result selector
-	//         mode: action mode
-    //         url: target url,
-    //         params: query params,
+    // config.name: Action name
+    // config.selector: result selector
+	// config.mode: action mode
+    // config.url: target url
+    // config.params: query params
 	_action: function(config) {
         config.params['bdajax.action'] = config.name;
 		config.params['bdajax.mode'] = config.mode;
@@ -256,7 +225,6 @@ bdajax = {
         });
 	},
 	
-	// Split str by ' ' and return Array
 	_to_array: function(str) {
         var arr;
 		if (str.indexOf(' ') != -1) {
