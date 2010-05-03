35fe087cd7b5e65540c18b49927a713472188592
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-05-03 15:32:50 +0000

    test utils
    git-svn-id: https://svn.bluedynamics.eu/svn/module/bda.bfg.app/trunk@2086 f119a143-da5b-4457-81e3-bee00661a780

--- src/bda/bfg/app/browser/utils.py
+++ src/bda/bfg/app/browser/utils.py
@@ -11,7 +11,10 @@ def make_query(**kw):
     for name, param in kw.items():
         if param is None:
             continue
-        query.append('%s=%s' % (name, param))
+        if isinstance(param, basestring):
+            param = [param]
+        for p in param:
+            query.append('%s=%s' % (name, p))
     return '?%s' % '&'.join(query)
 
 def make_url(request, path=[], node=None, resource=None, query=None):
@@ -22,42 +25,3 @@ def make_url(request, path=[], node=None, resource=None, query=None):
     if not query:
         return '%s/%s' % (request.application_url, '/'.join(path))
     return '%s/%s%s' % (request.application_url, '/'.join(path), query)
-
-class HTMLRenderer(object):
-    
-    def tag(self, name_, *args, **kw):
-        attrlist = list()
-        for key, value in kw.items():
-            if value is None:
-                continue
-            if not isinstance(value, unicode):
-                value = str(value).decode('utf-8')
-            attrlist.append((key, value))
-        attrs = u' '.join(u'%s="%s"' % (key.strip('_'), value) \
-                                          for key, value in attrlist)
-        attrs = attrs and u' %s' % attrs or u''
-        arglist = list()
-        for arg in args:
-            if not isinstance(arg, unicode):
-                arg = str(arg).decode('utf-8')
-            arglist.append(arg)
-        if not arglist:
-            return u'<%(name)s%(attrs)s />' % {
-                'name': name_,
-                'attrs': attrs,
-            }
-        return u'<%(name)s%(attrs)s>%(value)s</%(name)s>' % {
-            'name': name_,
-            'attrs': attrs,
-            'value': u''.join(c for c in arglist),
-        }
-    
-    def selection(self, vocab_, **kw):
-        options = list()
-        for term in vocab_:
-            optkw = {'value': term[0]}
-            if term[2]:
-                optkw['selected'] = 'selected'
-            option = self.tag('option', term[1], **optkw)
-            options.append(option)
-        return self.tag('select', *options, **kw)
