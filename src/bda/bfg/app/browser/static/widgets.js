107eb700549e72b727b6aeb04a4be6ea8290c54e
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-05-04 14:57:23 +0000

    git-svn-id: https://svn.bluedynamics.eu/svn/module/bda.bfg.app/trunk@2117 f119a143-da5b-4457-81e3-bee00661a780

---
+++ src/bda/bfg/app/browser/static/widgets.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function() {
+	jQuery('.dropdown').dropdownmenu();
+});
+
+// <div class="dropdown">
+//    <div class="icon">
+//      <a href="http://example.com">&nbsp;</a>
+//    </div>
+//    <ul class="dropdown_items" style="display:none;">
+//      <li>
+//        <a href="http://example.com/whatever">
+//          Item title
+//        </a>
+//      </li>
+//    </ul>
+//  </div>
+jQuery.fn.dropdownmenu = function () {
+	jQuery(this).unbind('click');
+    jQuery('.icon a', this).bind('click', function(event) {
+		event.preventDefault();
+		var container = jQuery('.dropdown_items',
+		                       jQuery(this).parent().parent());
+		jQuery(document).bind('mousedown', function(event) {
+            if (!event) {
+                var event = window.event;
+            }
+            if (event.target) {
+                var target = event.target;
+            } else if (event.srcElement) {
+                var target = event.srcElement;
+            }
+			if (!jQuery('.dropdown', jQuery(target)).get(0)) {
+				return true;
+			}
+            container.css('display', 'none');
+        });
+		container.css('display', 'block');
+	});
+}
