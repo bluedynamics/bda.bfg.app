cb20633fc3762bfc9dc367d1fce8b8caca9dc62c
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-05-08 11:02:50 +0000

    git-svn-id: https://svn.bluedynamics.eu/svn/module/bda.bfg.app/trunk@2209 f119a143-da5b-4457-81e3-bee00661a780

--- src/bda/bfg/app/browser/static/widgets.js
+++ src/bda/bfg/app/browser/static/widgets.js
@@ -1,28 +1,53 @@
 jQuery(document).ready(function() {
 	bdapp.dropdownmenubinder();
+	bdapp.datepickerbinder();
 	bdajax.binders.dropdownmenubinder = bdapp.dropdownmenubinder;
+	bdajax.binders.datepickerbinder = bdapp.datepickerbinder;
 });
 
 bdapp = {
 	
 	dropdownmenubinder: function(context) {
 	    jQuery('.dropdown').dropdownmenu();
-	}
+	},
 	
+	datepickerbinder: function(context) {
+        jQuery('input.datepicker').datepicker({
+            showOn: 'button',
+            buttonImage: '/static/icons/calendar16_16.gif',
+            buttonImageOnly: true,
+        });
+    }
 }
 
-// <div class="dropdown">
-//    <div class="icon">
-//      <a href="http://example.com">&nbsp;</a>
-//    </div>
-//    <ul class="dropdown_items" style="display:none;">
-//      <li>
-//        <a href="http://example.com/whatever">
-//          Item title
-//        </a>
-//      </li>
-//    </ul>
-//  </div>
+/* 
+ * Dropdown menu
+ * =============
+ * 
+ * Example markup:
+ * ---------------
+ * 
+ *     <div class="dropdown">
+ *       <div class="icon">
+ *         <a href="http://example.com">&nbsp;</a>
+ *       </div>
+ *       <ul class="dropdown_items" style="display:none;">
+ *         <li>
+ *           <a href="http://example.com/whatever">
+ *             Item title
+ *           </a>
+ *         </li>
+ *       </ul>
+ *     </div>
+ * 
+ * Example JS:
+ * -----------
+ * 
+ *     jQuery('.dropdown').dropdownmenu({
+ *         menu: '.dropdown_items',
+ *         trigger: '.icon a'
+ *     });
+ */
 jQuery.fn.dropdownmenu = function (options) {
 	var selector = this.selector;
 	var trigger = options ? (options.trigger ? options.trigger : '.icon a')
