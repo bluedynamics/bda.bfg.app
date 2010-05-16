36f5c169d6b51a236fa84a816286f7b0fbd5e04e
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-05-16 13:05:16 +0000

    git-svn-id: https://svn.bluedynamics.eu/svn/module/bda.bfg.app/trunk@2343 f119a143-da5b-4457-81e3-bee00661a780

--- src/bda/bfg/app/browser/referencebrowser.py
+++ src/bda/bfg/app/browser/referencebrowser.py
@@ -1,3 +1,12 @@
+from bda.bfg.tile import (
+    tile,
+    registerTile,
+)
+from bda.bfg.app.browser.layout import PathBar
+from bda.bfg.app.browser.contents import (
+    ContentsTile,
+    ContentsBatch,
+)
 from yafowil.base import (
     factory,
     UNSET,
@@ -15,15 +24,26 @@ from yafowil.utils import (
     cssid,
     cssclasses,
 )
-from bda.bfg.tile import (
-    tile,
-    registerTile,
-)
-from bda.bfg.app.browser.layout import PathBar
-from bda.bfg.app.browser.contents import (
-    ContentsTile,
-    ContentsBatch,
-)
+
+registerTile('referencebrowser',
+             'bda.bfg.app:browser/templates/referencebrowser.pt',
+             permission='view',
+             strict=False)
+
+registerTile('referencebrowser_pathbar',
+             'bda.bfg.app:browser/templates/referencebrowser_pathbar.pt',
+             permission='view',
+             class_=PathBar,
+             strict=False)
+
+@tile('referencelisting', 'templates/referencelisting.pt', strict=False)
+class ReferenceListing(ContentsTile):
+    
+    @property
+    def batch(self):
+        batch = ContentsBatch(self.contents)
+        batch.name = 'referencebatch'
+        return batch(self.model, self.request)
 
 def reference_extractor(widget, data):
     if widget.attrs.get('multivalued'):
@@ -65,23 +85,3 @@ factory.register(
         reference_renderer,
     ],
 )
-
-registerTile('referencebrowser',
-             'bda.bfg.app:browser/templates/referencebrowser.pt',
-             permission='view',
-             strict=False)
-
-registerTile('referencebrowser_pathbar',
-             'bda.bfg.app:browser/templates/referencebrowser_pathbar.pt',
-             permission='view',
-             class_=PathBar,
-             strict=False)
-
-@tile('referencelisting', 'templates/referencelisting.pt', strict=False)
-class ReferenceListing(ContentsTile):
-    
-    @property
-    def batch(self):
-        batch = ContentsBatch(self.contents)
-        batch.name = 'referencebatch'
-        return batch(self.model, self.request)
